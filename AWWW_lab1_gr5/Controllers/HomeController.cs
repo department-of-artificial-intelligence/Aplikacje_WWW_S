5723c0450b8e78075fbc8794fe45553e2e562a58
Dominik Nowak <dn@gmail.com>
2024-02-28 13:47:42 +0100

    Fix

--- AWWW_lab1_gr5/Controllers/HomeController.cs
+++ AWWW_lab1_gr5/Controllers/HomeController.cs
@@ -1,8 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-public class HomeController{
-    public IActionResult Index(){
-        ViewBag.Title = "Home"; 
-        return View();
+namespace AWWW_lab1_gr5.Controllers
+{
+        public class HomeController : Controller
+    {        public IActionResult Index()
+        {
+            ViewBag.Title = "Home"
+            return View();
+        }
     }
 }
