4c32225841858556cadc93c882d651f45c397caf
Nazar_Cheva <damelil@gmail.com>
2024-03-01 13:48:51 +0100

    naz

---
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -0,0 +1,7 @@
+using AWWW_lab1_gr1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class ArticleController : Controller {
+    public IActionResult Index
+}
+
---
+++ AWWW_lab1_gr1/Models/Articles.cs
@@ -0,0 +1,7 @@
+public class Article
+{
+    public int Id {get; set;}
+    public int Title {get; set;}
+    public int Content {get; set;}
+    public int CreationDate {get; set;}
+}
