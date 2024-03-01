8698139e9405c2bfc55c73b2327afe722173cddc
Leonid_Fabishvevkyi <km@km.pl>
2024-03-01 13:53:24 +0100

    lab1

--- AWWW_lab1_gr1/Controllers/ArticleController.cs
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 public class ArticleController:Controller
 {
-    public IActionResult Index()
+    public IActionResult Article()
     {
         ViewBag.Title = "Article";
         return View();
