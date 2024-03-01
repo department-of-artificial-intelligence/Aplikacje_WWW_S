68636f3f1c98cbbb2e66a0efcc6c784730edb5ff
Marek_Jaskowiak <mj6238819123@gmail.com>
2024-03-01 13:47:00 +0100

    lab1 starting

---
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using AWWW_lab1_gr1.Models;
+
+public class ArticleController : Controller
+    {
+
+public IActionResult Index(int id = 1)
+{
+    
+    var articles = new List<Article>{
+        new Article{
+        Id = 1,
+        Title = "Artykuł1",
+        Content = "Yaddy yadda",
+        CreationDate = DateTime.Now
+        },
+        new Article{
+        Id = 2,
+        Title = "Artykuł2",
+        Content = "Fafafafa fafafafafa faaaa",
+        CreationDate = DateTime.Now
+        },
+        new Article{
+        Id = 3,
+        Title = "Artykuł3",
+        Content = "Diddly doo",
+        CreationDate = DateTime.Now
+        },
+    };
+
+    return View(articles[id-1]);
+}
+
+    }
