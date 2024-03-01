22cb2ee86f686614b4e9a0847397812463169083
Kacper_Zawadzki <you@example.com>
2024-03-01 11:44:17 +0100

    Laboratoria_1

---
+++ AWWW_lab1_gr2/Controllers/ArticleController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using AWWW_lab1_gr2.Models;
+
+namespace AWWW_lab1_gr2.Controllers{
+    public class ArticleController :Controller{
+        public IActionResult Index(){
+            var article = new Article
+            {
+                Id = 1,
+                Title = "Artykuł 1",
+                Content = "Lorem ipsum...",
+                CreationDate = DateTime.Now
+            };
+            return View(article);
+        }
+        /*
+        public IActionResult Index(int id=1){
+            var articles = new List<Article>
+            {
+                new Article{
+                    Id = 1,
+                    Title = "Artykuł 1",
+                    Content = "Lorem ipsum...",
+                    CreationDate = DateTime.Now
+                },
+                new Article{
+                    Id = 2,
+                    Title = "Artykuł 2",
+                    Content = "Lorem ipsum...",
+                    CreationDate = DateTime.Now
+                },
+                new Article{
+                    Id = 3,
+                    Title = "Artykuł 3",
+                    Content = "Lorem ipsum...",
+                    CreationDate = DateTime.Now
+                }
+            };
+            return View(articles[id-1]);
+        }
+        */
+    }
+
+}
---
+++ AWWW_lab1_gr2/Program.cs
@@ -0,0 +1,12 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.MapDefaultControllerRoute();
+//app.MapGet("/", () => "Hello World!");
+
+
+app.Run();
