3a36a530d82fdc8b6f025c99cd1e472077bb227a
jakub_jakubowski <jakubowski@jj.com>
2024-03-01 12:49:05 +0100

    lab1 zad1-19

---
+++ AWWW_lab1_gr2/Controllers/ArticleController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+public class ArticleController :Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.Title="Home";
+        return View();
+    }
+}
