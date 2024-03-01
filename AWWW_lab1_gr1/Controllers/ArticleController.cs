18aa35fcc10a0a510888316a0ed93c361410311a
Leonid_Fabishvevkyi <km@km.pl>
2024-03-01 13:42:37 +0100

    lab1error

---
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class ArticleController:Controller
+{
+    public IActionResult Index()
+    {
+        ViewBag.Title = "Article";
+        return View();
+    }
+}
