d1f09d01c1a534a7302c65446d1e8fde1e56a50e
Kamil_Czudaj <smth@smth.smth>
2024-03-01 13:50:32 +0100

    lab01

---
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class ArticleController : Controller
+{
+    public IActionResult Index()
+    {
+        var article = new Article
+        {
+            Id = 1,
+            Title = "Artykuł 1",
+            Content = "Lorem ipsum...",
+            CreationDate = DateTime.Now
+        };
+
+        return View(article);
+    }
+}
