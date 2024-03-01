b4a9c4ff824f17cc10cba76ab741ee75ecb26dca
Illia_Nieniunkin <w35yw54ehe4we4hy>
2024-03-01 13:50:19 +0100

    AWWW_lab1_gr1

---
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -0,0 +1,19 @@
+using AWWW_lab1_gr1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+    public class ArticleController : Controller{
+        public IActionResult Index()
+        {
+            var article = new Article
+            {
+                Id = 1,
+                Title = "Artykuł 1",
+                Content = "Lorem ipsum...",
+                CreationDate = DateTime.Now
+            };
+
+            return View(article);
+        }
+    }
+
