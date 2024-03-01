1c1e2ffe5d61cee74a15b19b02ac852e8cae67eb
g1xx <nenilgen@gmail.com>
2024-03-01 16:27:12 +0100

    Illia_Nieniunkin

--- AWWW_lab1_gr1/Controllers/ArticleController.cs
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -1,19 +1,32 @@
 using AWWW_lab1_gr1.Models;
 using Microsoft.AspNetCore.Mvc;
 
-
     public class ArticleController : Controller{
-        public IActionResult Index()
+        public IActionResult Index(int id=1)
         {
-            var article = new Article
+            var articles = new List<Article>
             {
+                new Article{
                 Id = 1,
                 Title = "Artykuł 1",
                 Content = "Lorem ipsum...",
                 CreationDate = DateTime.Now
+                },
+                new Article{
+                Id = 2,
+                Title = "Artykuł 2",
+                Content = "Lorem ipsum2...",
+                CreationDate = DateTime.Now
+                },
+                new Article{
+                Id = 3,
+                Title = "Artykuł 3",
+                Content = "Lorem ipsum3...",
+                CreationDate = DateTime.Now
+                }
             };
 
-            return View(article);
+            return View(articles[id-1]);
         }
     }
 
