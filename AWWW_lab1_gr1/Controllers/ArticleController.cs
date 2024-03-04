a3c4a4a76f6cb79b9cd0e890b3b8e528cd57e2ab
Artur_Bula <exp@exp.com>
2024-03-04 10:06:58 +0100

    update

--- AWWW_lab1_gr1/Controllers/ArticleController.cs
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -1,17 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
-
 public class ArticleController : Controller
 {
 
-    public IActionResult Index()
+    public IActionResult Index(int id = 1)
     {
-        var article = new Article
+        var articles = new List<Article>()
+        {
+
+        new Article
         {
             Id = 1,
             Title = "artykuł 1",
             Content = "lorem impsu",
             CreationDate = DateTime.Now
+        },
+        new Article
+        {
+            Id =2,
+            Title = "Artykuł 2",
+            Content = "lorem ",
+            CreationDate = DateTime.Now
+                    },
+        new Article
+        {
+            Id =3,
+            Title = "Artykuł 3",
+            Content = "lorem ",
+            CreationDate = DateTime.Now
+        }
         };
-        return View(article);
+        return View(articles[id - 1]);
     }
 }
