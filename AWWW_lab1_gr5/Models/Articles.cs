52724b29ba24fe31b7e666786263d55dbec95b2a
Kamil_Drzazga <kd@kdkd.kd>
2024-03-05 21:19:51 +0100

    notNullableFix

--- AWWW_lab1_gr5/Models/Articles.cs
+++ AWWW_lab1_gr5/Models/Articles.cs
@@ -3,8 +3,8 @@ namespace AWWW_lab1_gr5.Models
     public class Article
     {
         public int Id {get; set;}
-        public string Title { get; set;}
-        public string Content { get; set;}
+        public string? Title { get; set;}
+        public string? Content { get; set;}
 
         public DateTime CreationDate {get; set;}
 
