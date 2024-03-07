98764fff3c7627a547b5e65f530891a1519ffc0d
Egorgor <egorgor28@gmail.com>
2024-03-08 01:10:12 +0200

    lab1

--- AWWW_lab1_gr1/Controllers/ArticleController.cs
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -1,12 +1,34 @@
-using System.Security.Cryptography;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
 using AWWW_lab1_gr1.Models;
-public class ArticleController:Controller{
-    
-    public IActionResult Article(){
-        ViewBag.Title = "article ";
-        
-        return View();
+
+public class ArticleController:Controller
+{
+    public IActionResult Index(int id=1)
+    {
+       var articles = new List<Article> 
+        { 
+            new Article{ 
+            ID = 1, 
+            Title = "Artykul 1", 
+            Content = "Lorem ipsum...", 
+            CreationDate = DateTime.Now 
+            }, 
+            new Article{ 
+            ID = 2, 
+            Title = "Artykul 2", 
+            Content = "Lorem ipsum...", 
+            CreationDate = DateTime.Now 
+            }, 
+            new Article{ 
+            ID = 3, 
+            Title = "Artykul 3", 
+            Content = "Lorem ipsum...", 
+            CreationDate = DateTime.Now 
+            } 
+         
+   
+        };
+
+        return View(articles[id-1]);
     }
 }
--- AWWW_lab1_gr1/Program.cs
+++ AWWW_lab1_gr1/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
