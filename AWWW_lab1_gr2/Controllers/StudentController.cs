b62e53b833c34b10aacaa293ce1c3e4b64c874f1
Patryk_Wawrzak <emai@mailowy.com>
2024-03-07 23:01:54 +0100

    Lab 1 mniej ugh

--- AWWW_lab1_gr2/Controllers/StudentController.cs
+++ AWWW_lab1_gr2/Controllers/StudentController.cs
@@ -1,14 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 
 public class StudentController : Controller{
-    public IActionResult Index(int id=1){
+    
+    public List<Student> createStudents(){
         var students = new List<Student>{
             new Student{
                 Id=1,
                 FirstName="Name 1",
                 LastName="Surname 1",
                 IndexNr=111111,
-                DateOfBirth=new DateTime(2001,01,01),
+                DateOfBirth=new DateTime(01/01/2001),
                 FieldOfStudy="Informatyka"
             },
             new Student{
@@ -16,7 +17,7 @@ public class StudentController : Controller{
                 FirstName="Name 2",
                 LastName="Surname 2",
                 IndexNr=222222,
-                DateOfBirth=new DateTime(2002,02,02),
+                DateOfBirth=new DateTime(02/02/2002),
                 FieldOfStudy="Matematyka"
             },
             new Student{
@@ -24,10 +25,19 @@ public class StudentController : Controller{
                 FirstName="Name 3",
                 LastName="Surname 3",
                 IndexNr=333333,
-                DateOfBirth=new DateTime(2003,03,03),
+                DateOfBirth=new DateTime(03/03/2003),
                 FieldOfStudy="Budownictwo"
             }
         };
+        return students;
+    }
+    
+    public IActionResult Index(){
+        return View(createStudents());
+    }
+    
+    public IActionResult Details(int id){
+        var students = createStudents();
         return View(students[id-1]);
     }
 }
