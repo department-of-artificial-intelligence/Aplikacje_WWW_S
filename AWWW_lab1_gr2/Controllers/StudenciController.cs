94d3d75c93a45dea4c37ea6aed23765f520beccd
jakub_jakubowski <jakubowski@jj.com>
2024-03-08 12:30:37 +0100

    32v2

---
+++ AWWW_lab1_gr2/Controllers/StudenciController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+namespace AWWW_lab1_gr2.Controllers
+{
+    public class StudenciController
+    {
+         public IActionResult Index(int id=1)
+    {
+        var Studenci = new List<Student>
+        {
+            new Student
+            {
+                Id=1,
+                Firstname="jakub"
+            },
+
+
+        };
+       
+       return View(Studenci[id-1]);
+    }
+    }
+}
