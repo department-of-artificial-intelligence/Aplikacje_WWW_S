18963f0111a77ce5d930d058790b9a9164e8f894
Marceli_Nowak <kt2137_69@gmail.com>
2024-03-08 10:39:17 +0100

    Pierwsze modele
    Z lab2

---
+++ AWWW_lab1_gr2/Models/Author.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWWW_lab1_gr2.Models
+{
+    public class Author
+    {
+        public int ID{get;set;}
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+    }
+}
---
+++ AWWW_lab1_gr2/Models/Category.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWWW_lab1_gr2.Models
+{
+    public class Category
+    {
+        public int ID{get;set;}
+        public String Name{get;set;}
+    }
+}
