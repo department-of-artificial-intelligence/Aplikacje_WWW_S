d3e0dc9aafbd612c6cb53b177a67a0c2b5e96a4b
Marceli <ert@gmail.com>
2024-03-08 20:22:49 +0100

    Test

---
+++ AWWW_lab1_gr2/Controllers/TagController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using AWWW_lab1_gr2.Models;
+
+public class ArticleController:Controller{
+    public IActionResult Index(int itemid){
+
+        var articles = new List<Article>{
+            new Article{
+                Id = 1,
+                Title = "Artykuł 1",
+                Content = "Lorem ispum...",
+                CreationDate = DateTime.Now
+            },
+
+            new Article{
+                Id = 2,
+                Title = "Artykuł 2",
+                Content = "Lorem ispum...",
+                CreationDate = DateTime.Now
+            },
+
+            new Article{
+                Id = 3,
+                Title = "Artykuł 3",
+                Content = "Lorem ispum...",
+                CreationDate = DateTime.Now
+            }
+        };
+        return View(articles[itemid-1]);
+    }
+}
