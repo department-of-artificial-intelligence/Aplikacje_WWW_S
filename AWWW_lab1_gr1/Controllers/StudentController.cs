da562a4629dd37e68a7946d64b39756aed9adc03
Oleksii_Khoruzhyi <asdasdasd@gmail.com>
2024-03-13 00:00:46 +0100

    test5

--- AWWW_lab1_gr1/Controllers/StudentController.cs
+++ AWWW_lab1_gr1/Controllers/StudentController.cs
@@ -1,31 +1,44 @@
-using AWWW_lab1_gr1.Models;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; 
+using AWWW_lab1_gr1.Models; 
 
-public class StudentController : Controller{
-public IActionResult Index(int id = 1)
+public class StudentController:Controller
 {
-    var student = new List<Students>
+    public ActionResult Index(int id=1)
     {
-        new Article{
-            Id = 1,
-            Title = "Artykul 1",
-            Content = "Lorem ipsum...",
-            CreationDate = DateTime.Now
-        },
-        new Article{
-            Id = 2,
-            Title = "Artykul 2",
-            Content = "Lorem ipsum...",
-            CreationDate = DateTime.Now
-        },
-        new Article{
-            Id = 3,
-            Title = "Artykul 3",
-            Content = "Lorem ipsum...",
-            CreationDate = DateTime.Now
-        }
-    };
-    return View(articles[id-1]);
-    
-}
+        var student = new List<Student>
+        {
+            new Student 
+            {
+                ID = 1, 
+                FirstName = "Naz", 
+                LastName = "James",
+                IndexNr = 1337, 
+                DateOfBirth = new DateTime(2004,3,12),
+                FieldOfStudy = "Math"
+            },
+
+            new Student 
+            {
+                ID = 2, 
+                FirstName = "Nilgen", 
+                LastName = "Nu",
+                IndexNr = 1488, 
+                DateOfBirth = new DateTime(2003,2,26),
+                FieldOfStudy = "Engl"
+            },
+
+            new Student 
+            {
+                ID = 3, 
+                FirstName = "Kasha", 
+                LastName = "Porter",
+                IndexNr = 2289, 
+                DateOfBirth = new DateTime(2008,4,2),
+                FieldOfStudy = "Fizyka"
+            }
+
+
+        };
+        return View(student[id-1]);
+    }
 }
