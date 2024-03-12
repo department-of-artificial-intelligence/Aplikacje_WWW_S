9fd1aec73cb2f3473f384bdf278e4a982b7a056a
Oleksii_Khoruzhyi <asdasdasd@gmail.com>
2024-03-12 22:38:36 +0100

    Lab1_new_commit

--- AWWW_lab1_gr1/Controllers/ArticleController.cs
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -2,15 +2,30 @@ using AWWW_lab1_gr1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 public class ArticleController : Controller{
-public IActionResult Index()
+public IActionResult Index(int id = 1)
 {
-    var article = new Article
+    var articles = new List<Article>
     {
-        Id = 1,
-        Title = "Article",
-        Content = "Lorem ipsum...",
-        CreationDate = DateTime.Now
+        new Article{
+            Id = 1,
+            Title = "Artykul 1",
+            Content = "Lorem ipsum...",
+            CreationDate = DateTime.Now
+        },
+        new Article{
+            Id = 2,
+            Title = "Artykul 2",
+            Content = "Lorem ipsum...",
+            CreationDate = DateTime.Now
+        },
+        new Article{
+            Id = 3,
+            Title = "Artykul 3",
+            Content = "Lorem ipsum...",
+            CreationDate = DateTime.Now
+        }
     };
-    return View(article);
+    return View(articles[id-1]);
+    
 }
 }
--- AWWW_lab1_gr1/Models/Article.cs
+++ AWWW_lab1_gr1/Models/Article.cs
@@ -2,8 +2,8 @@ namespace AWWW_lab1_gr1.Models;
 public class Article
 {
     public int Id {get; set;}
-    public string Title {get; set;}
-    public string Content{get; set;}
+    public string Title {get; set;} = "";
+    public string Content{get; set;} = "";
     public DateTime CreationDate{get; set;}
 
 }
