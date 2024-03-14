9195573e06898d2bffd96f901154bd7b57e2d701
Damian_Matysiak <Damian@DESKTOP-GB02VA0>
2024-03-14 23:12:17 +0100

    lab2_w_domu

---
+++ AWWW_lab2_gr2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using AWWW_lab2_gr2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWWW_lab2_gr2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
