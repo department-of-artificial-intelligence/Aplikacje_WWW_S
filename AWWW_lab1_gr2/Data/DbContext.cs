3f08827349b9d4346dd4b12d92a152a67bb8f242
MarsonMarson <nowak14marceli@gmail.com>
2024-03-15 02:18:33 +0100

    Lab2 prawie

---
+++ AWWW_lab1_gr2/Data/DbContext.cs
@@ -0,0 +1,36 @@
+using System.Data.Entity;
+namespace AWWW_lab1_gr2.Models
+{
+    public class DbContext:DbContext
+    {
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<EventType> EventTypes { get; set; }
+        public DbSet<League> Leagues { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<MatchEvent> MatchEvents { get; set; }
+        public DbSet<MatchPlayer> MatchPlayers { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+            modelBuilder.Entity<Match>()
+            .HasOne(m => m.HomeTeam)
+            .WithMany(t => t.HomeMatches)
+            .HasForeignKey(m => m.HomeTeamId)
+            .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<Match>()
+            .HasOne(m => m.AwayTeam)
+            .WithMany(t => t.AwayMatches)
+            .HasForeignKey(m => m.AwayTeamId)
+            .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
