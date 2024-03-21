e592ff6a8e914ee071ac307532e834dcaaf06689
Oleksii_Khoruzhyi <asdasdasd@gmail.com>
2024-03-21 20:47:58 +0100

    Lab_pre_db

--- AWWW_lab1_gr1/Models/Author.cs
+++ AWWW_lab1_gr1/Models/Author.cs
@@ -7,5 +7,5 @@ public class Author{
     public int AuthorId{get; set;}
     public string FirtstName{get;set;} = ""; 
     public string LastName{get;set;} ="";
-    public ICollection<Article> Articles{get;set;}
+    public List<Article> Articles{get;set;}
 }
--- AWWW_lab1_gr1/Models/EventType.cs
+++ AWWW_lab1_gr1/Models/EventType.cs
@@ -1,7 +1,9 @@
 namespace AWWW_lab1_gr1.Models;
 
-public class EventType{
-    public int EventTypeId{get;set;}
-    public string Name{get;set;} = "";
+public class EventType {
+    public int Id { get; set; }
 
+    public string Name { get; set; }
+
+    public List<MatchEvent> MatchEvents { get; set; }
 }
