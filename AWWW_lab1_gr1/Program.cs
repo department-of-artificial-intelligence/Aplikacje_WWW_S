01db2526a1bb949f73747ae4a9c2c3ca9a8c25d1
Oleksii_Khoruzhyi <asdasdasd@gmail.com>
2024-03-21 21:31:22 +0100

    lab_js_fix

--- AWWW_lab1_gr1/Program.cs
+++ AWWW_lab1_gr1/Program.cs
@@ -1,12 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<DbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DatabaseStringContext")));
+
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<DbContext>();
+    context.Database.EnsureCreated();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 
 app.MapDefaultControllerRoute();
+
+
+
+
 
 
 app.Run();
