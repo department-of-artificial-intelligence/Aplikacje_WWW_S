f2ba8e074c6fdb3093bb951d955fc163f95756da
Miraslau_Piatselchyts <mirosslav11@gmail.com>
2024-03-22 13:35:57 +0100

    lab

--- AWWW_lab1_gr1/Controlles/ArticleController.cs
+++ AWWW_lab1_gr1/Controlles/ArticleController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using AWWW_lab1_gr1.Models;
+using Microsoft.EntityFrameworkCore;
 
 
  public class ArticleController:Controller{
@@ -7,28 +8,39 @@ using AWWW_lab1_gr1.Models;
     public IActionResult Index(int id=1)
     {
         var articles = new List<Article>
-        {
+        {/*
             new Article{
             ID = 1,
             Title = "Artykul 1",
             Content = "Lorem ipsum",
-            CreationDate = DateTime.Now
-    
+            CreationDate = DateTime.Now,
+            Lead = "",
+            Category= new Category(){Articles=new List<Article>(),Name="sadsa"},
+            Comments=new List<Comment>(){},
+            Tags = new List<Tag>(){}
             },
             new Article{
             ID = 2,
             Title = "Artykul 2",
             Content = "Lorem ipsum",
-            CreationDate = DateTime.Now
+            CreationDate = DateTime.Now,
+            Lead = "",
+              Category= new Category(){Articles=new List<Article>(),Name="sadsa"},
+            Comments=new List<Comment>(){},
+            Tags = new List<Tag>(){}
             },
             new Article{
             ID = 3,
             Title = "Artykul 3",
             Content = "Lorem ipsum",
-            CreationDate = DateTime.Now
+            CreationDate = DateTime.Now,
+            Lead = "",
+            Category= new Category(){Articles=new List<Article>(),Name="sadsa"},
+            Comments=new List<Comment>(){},
+            Tags = new List<Tag>(){}
             }
         
-  
+  */
         };
         return View(articles[id-1]);
     }
--- AWWW_lab1_gr1/Models/Author.cs
+++ AWWW_lab1_gr1/Models/Author.cs
@@ -1,4 +1,5 @@
 namespace AWWW_lab1_gr1.Models;
+using Microsoft.EntityFrameworkCore;
 public class Author{
     public required int ID{get; set;}
     public required string FirstName{get; set;}
