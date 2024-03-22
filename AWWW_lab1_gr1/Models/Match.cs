0b1acd0cc62bc9e43f69080e377f40074bbe8b9b
Miraslau_Piatselchyts <mirosslav11@gmail.com>
2024-03-22 13:53:17 +0100

    lab1

--- AWWW_lab1_gr1/Models/Match.cs
+++ AWWW_lab1_gr1/Models/Match.cs
@@ -1,7 +1,17 @@
 namespace AWWW_lab1_gr1.Models;
-public class Match{
-    public int ID{get; set;}
-    public DateTime Date{get;set;}
-    public required string Stadium{get;set;}
-  
+public class Match
+{
+        public Match(int id) 
+        {
+            this.Id = id;
+   
+        }
+            public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public virtual League League { get; set; }
+    public virtual ICollection<MatchEvent> MatchEvents { get; set; }
+    public virtual Team HomeTeam{get; set;} = null!;
+    public virtual Team AwayTeam{get; set;} = null!;
+    public int HomeTeamId { get; internal set; }
+    public int AwayTeamId { get; internal set; }
 }
