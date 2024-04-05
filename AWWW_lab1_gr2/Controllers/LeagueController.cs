1b996453f901ba326408c14d7a0aa8ffb3b5dfad
Marsony <emailemail@email.email>
2024-04-05 02:43:48 +0200

    Cosdzialo

--- AWWW_lab1_gr2/Controllers/LeagueController.cs
+++ AWWW_lab1_gr2/Controllers/LeagueController.cs
@@ -1,11 +1,49 @@
 using Microsoft.AspNetCore.Mvc;
 using AWWW_lab1_gr2.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 
 public class LeagueController:Controller{
-    public IActionResult Index()
+    private readonly MyDbContext context;
+    public LeagueController(MyDbContext conteext){
+        context = conteext;
+    }
+    public async Task<IActionResult> Index()
     {
         ViewBag.Title = "Ligi";
+        var data = await context.Leagues.ToListAsync();
+        return View(data);
+    }
+
+    [HttpPost]
+    public IActionResult Added(League league)
+    {
+        if (ModelState.IsValid)
+        {
+            context.Leagues.Add(league);
+            context.SaveChanges();
+            return RedirectToAction("Index", "League");
+        }
+        return View(league);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UsunDane(int id)
+    {
+        var dane = await context.Leagues.FindAsync(id);
+        if (dane == null)
+        {
+            return NotFound();
+        }
+        context.Leagues.Remove(dane);
+        await context.SaveChangesAsync();
+
+        return RedirectToAction("Index"); // Przekierowanie na widok po usunięciu danych
+    }
+
+    public IActionResult Add(){
         return View();
     }
 }
--- AWWW_lab1_gr2/MyDbContext.cs
+++ AWWW_lab1_gr2/MyDbContext.cs
@@ -11,18 +11,18 @@ using Microsoft.EntityFrameworkCore;
         {
         }
 
-        public DbSet<Article> Articles { get; set; }
-        public DbSet<Author> Authors { get; set; }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<EventType> EventTypes { get; set; }
+        //public DbSet<Article> Articles { get; set; }
+        //public DbSet<Author> Authors { get; set; }
+        //public DbSet<Category> Categories { get; set; }
+        //public DbSet<Comment> Comments { get; set; }
+        //public DbSet<EventType> EventTypes { get; set; }
         public DbSet<League> Leagues { get; set; }
         public DbSet<Match> Matches { get; set; }
-        public DbSet<MatchEvent> MatchEvents { get; set; }
-        public DbSet<MatchPlayer> MatchPlayers { get; set; }
+        //public DbSet<MatchEvent> MatchEvents { get; set; }
+        //public DbSet<MatchPlayer> MatchPlayers { get; set; }
         public DbSet<Player> Players { get; set; }
-        public DbSet<Position> Positions { get; set; }
-        public DbSet<Tag> Tags { get; set; }
+        //public DbSet<Position> Positions { get; set; }
+        //public DbSet<Tag> Tags { get; set; }
         public DbSet<Team> Teams { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
