7c08e94b91273ffc79d454d184e44eb2d9e6f244
Krus <Stutrjhjhdent@LAB-515es5e45dfdgt>
2024-04-12 11:37:44 +0200

    Lab3KoniecZajec

--- AWWW_lab3_gr2/AWWW_lab3_gr2/Controllers/PlayerController.cs
+++ AWWW_lab3_gr2/AWWW_lab3_gr2/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using AWWW_lab3_gr2.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace AWWW_lab3_gr2.Controllers
 {
@@ -14,7 +15,8 @@ namespace AWWW_lab3_gr2.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var players = _context.Players.Include(a => a.Team).ToList();
+            return View(players);
         }
 
         public IActionResult Add()
@@ -58,5 +60,55 @@ namespace AWWW_lab3_gr2.Controllers
             _context.SaveChanges();
             return View("Added", player);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ViewBag.teamsList = new SelectList(_context.Teams, "Id", "Name");
+            ViewBag.allPositions = _context.Positions.ToList();
+
+            var player = _context.Players
+                 .Include(p => p.Positions) // Załaduj pozycje za pomocą Eager Loading
+                 .FirstOrDefault(p => p.Id == id);
+
+            return View(player);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Player player, List<int> selectedPositions)
+        {
+            var playerToUpdate = _context.Players
+                .Include(p => p.Positions)
+                .FirstOrDefault(p => p.Id == player.Id);
+
+            if (playerToUpdate != null)
+            {
+                playerToUpdate.FirstName = player.FirstName;
+                playerToUpdate.LastName = player.LastName;
+                playerToUpdate.Country = player.Country;
+                playerToUpdate.BirthDate = player.BirthDate;
+                playerToUpdate.TeamId = player.TeamId;
+
+                playerToUpdate.Positions.Clear();
+                foreach (var positionId in selectedPositions)
+                {
+                    var positionToAdd = _context.Positions.Find(positionId);
+                    if (positionToAdd != null)
+                    {
+                        playerToUpdate.Positions.Add(positionToAdd);
+                    }
+                }
+            }
+            _context.SaveChanges();
+
+            var players = _context.Players
+                   .Include(p => p.Team)
+                   .Include(p => p.Positions);
+
+            return View("Index", players);
+        }       
     }
 }
