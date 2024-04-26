032d4a94b505b7dbe91dc9696570fab3a5715c4e
mikolajarzabkiewicz <mjarzabkiewicz.mikolaj@gmail.com>
2024-04-26 11:43:50 +0200

    lab4_koniez_zajec

---
+++ AWWW_lab4_gr2/SchoolRegister.Model/DataModels/Subject.cs
@@ -0,0 +1,14 @@
+﻿namespace SchoolRegister.Model.DataModels
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public IList<SubjectGroup> SubjectGroups { get; set; } = new List<SubjectGroup>();
+        public Teacher? Teacher { get; set; }
+        public int? TeacherId { get; set; }
+        public IList<Grade> Grades { get; set; } = new List<Grade>();
+
+    }
+}
