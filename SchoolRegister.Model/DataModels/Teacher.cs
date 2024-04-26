93c918048bd868fa549312a9ff935f74dc5318f4
Adrian_Sarna <asddas231d@gmail.com>
2024-04-26 11:47:36 +0200

    lab4

---
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.Model.DataModels
+{
+    public class Teacher
+    {
+        public IList<Subject> Subjects{get; set;}
+        public string Title{get; set;}=null!;
+        
+    }
+}
