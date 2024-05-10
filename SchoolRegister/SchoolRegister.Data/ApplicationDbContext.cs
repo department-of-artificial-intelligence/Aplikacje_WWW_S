1e571fb0b246864bd5e2be09a75797a78b412594
Ihor_Horshkov <igorgorshkov@wp.pl>
2024-05-10 14:49:46 +0200

    Artur: lab5 commit

---
+++ SchoolRegister/SchoolRegister.Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolRegister.Model;
+
+namespace SchoolRegister.Core.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<SubjectGroup> SubjectGroups { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>()
+                .ToTable("AspNetUsers")
+                .HasDiscriminator<int>("UserType")
+                .HasValue<User>((int)RoleValue.User)
+                .HasValue<Student>((int)RoleValue.Student)
+                .HasValue<Parent>((int)RoleValue.Parent)
+                .HasValue<Teacher>((int)RoleValue.Teacher);
+
+            builder.Entity<Grade>()
+                .HasKey(c => new { c.StudentId, c.DateOfIssue, c.SubjectId });
+
+            builder.Entity<SubjectGroup>()
+                .HasKey(c => new { c.SubjectId, c.GroupId });
+        }
+    }
+}
--- SchoolRegister/SchoolRegister.Web/Program.cs
+++ SchoolRegister/SchoolRegister.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolRegister.Core.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(
+    opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
