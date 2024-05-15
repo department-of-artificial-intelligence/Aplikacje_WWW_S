d530fd2f77b68071de2c54981b803d8aafbdc26a
Dominik Nowak <dn@gmail.com>
2024-05-15 13:26:03 +0200

    UnitTests

---
+++ AWWW_Lab4_gr5/SchoolRegister.ViewModels/VM/StudentVm.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class StudentVm
+    {
+        
+    }
+}
