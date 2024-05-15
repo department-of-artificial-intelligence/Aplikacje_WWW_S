c1b3a3f02a13d30dea78235258f46f3915801472
Mateusz <matlgrud@gmail.com>
2024-05-15 16:25:32 +0200

    Lab5 point 19 success compilation

--- AWWW_lab4_gr1/SchoolRegister.DAL/EF/ApplicationDbContext.cs
+++ AWWW_lab4_gr1/SchoolRegister.DAL/EF/ApplicationDbContext.cs
@@ -1,32 +1,57 @@
-using System.Text.RegularExpressions;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolRegister.Model.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+namespace SchoolRegister.DAL.EF
 {
-    //table properties
-    public DbSet<Grade> Grades { get; set; }
-    public DbSet<Group> Groups { get; set; }
-    public DbSet<Subject> Subjects { get; set; }
-    public DbSet<SubjectGroup> SubjectGroups { get; set; }
-
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public class ApplicationDbContext : IdentityDbContext<User, Role, int>
     {
-        base.OnConfiguring(optionsBuilder);
-        //configuration commands
-        optionsBuilder.UseLazyLoadingProxies(); //enable lazy loading proxies
-    }
+        //table properties
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<SubjectGroup> SubjectGroups { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-        //Fluent API commands
-        modelBuilder.Entity<User>()
-            .ToTable("AspNetUsers")
-            .HasDiscriminator<int>("UserType")
-            .HasValue<User>((int)RoleValue.User)
-            .HasValue<Student>((int)RoleValue.Student)
-            .HasValue<Parent>((int)RoleValue.Parent)
-            .HasValue<Teacher>((int)RoleValue.Teacher);
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            //configuration commands
+            optionsBuilder.UseLazyLoadingProxies(); //enable lazy loading proxies
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //Fluent API commands
+            modelBuilder.Entity<User>()
+                .ToTable("AspNetUsers")
+                .HasDiscriminator<int>("UserType")
+                .HasValue<User>((int)RoleValue.User)
+                .HasValue<Student>((int)RoleValue.Student)
+                .HasValue<Parent>((int)RoleValue.Parent)
+                .HasValue<Teacher>((int)RoleValue.Teacher);
+
+            modelBuilder.Entity<SubjectGroup>()
+                .HasKey(sg => new { sg.GroupId, sg.SubjectId });
+
+            modelBuilder.Entity<SubjectGroup>()
+                .HasOne(g => g.Group)
+                .WithMany(sg => sg.SubjectGroups)
+                .HasForeignKey(g => g.GroupId);
+
+            modelBuilder.Entity<SubjectGroup>()
+                .HasOne(g => g.Subject)
+                .WithMany(sg => sg.SubjectGroups)
+                .HasForeignKey(s => s.SubjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+            }
+
     }
 }
