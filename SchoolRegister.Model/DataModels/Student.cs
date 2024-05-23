48fe26d4d64b7ec7cdbdfc5243b73845ab745317
Nazar <fsdfdsf@gmail.com>
2024-05-24 00:01:13 +0200

    lab5_update

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -3,23 +3,28 @@ using System;
 namespace SchoolRegister.Model.DataModels;
 public class Student : User
 {
-    public Group Group {get; set;} 
+    public virtual Group? Group {get; set;}
+     [ForeignKey("Group")]     
     public int? GroupId {get;set;}
-    public IList<Grade> Grades {get;set;}
-    public Parent Parent { get;set;}
+    public virtual IList<Grade> Grades {get;set;} = default!;
+    public virtual Parent? Parent { get;set;}
+    [ForeignKey("Parent")] 
     public int? ParentId {get; set;}
-    public double AverageGrade {get;}
-    public IDictionary<string,double> AverageGradePerSubject {
-        get {
-            return Grades.GroupBy(g => g.Subject)
-                         .ToDictionary(g => g.Key, g => g.Average(v => (double)v.Value));
-        }
-    }
-    public IDictionary<string,List<GradeScale>> GradesPerSubject {
-        get {
-            return Grades.GroupBy(g => g.Subject)
-                         .ToDictionary(g => g.Key, g => g.Select(v => v.Scale).ToList());
-        }
-        }
+    [NotMapped] 
+    public double AverageGrade => Grades == null || Grades.Count == 0 ? 0.0d :  
+Math.Round(Grades.Average(g => (int)g.GradeValue), 1); 
+ 
+    [NotMapped] 
+    public IDictionary<string, double> AverageGradePerSubject => Grades == null ? new Dictionary<string, double>() : 
+        Grades.GroupBy(g => g.Subject.Name) 
+        .Select(g => new { SubjectName = g.Key, AvgGrade = Math.Round(g.Average(avg => (int)avg.GradeValue), 1) }) 
+        .ToDictionary(avg => avg.SubjectName, avg => avg.AvgGrade); 
+ 
+    [NotMapped] 
+    public IDictionary<string, List<GradeScale>> GradesPerSubject => Grades == null ? new Dictionary<string,  
+List<GradeScale>>() : Grades 
+        .GroupBy(g => g.Subject.Name) 
+        .Select(g => new { SubjectName = g.Key, GradeList = g.Select(x => x.GradeValue).ToList() }) 
+        .ToDictionary(x => x.SubjectName, x => x.GradeList); 
 
 }
---
+++ SchoolRegister.ViewModels/VM/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolRegister.ViewModels.VM
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
