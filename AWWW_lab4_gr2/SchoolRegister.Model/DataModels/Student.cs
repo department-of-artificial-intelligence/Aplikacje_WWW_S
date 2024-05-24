7711b5feb3b6ecf12bb3746324f8e07b7f7794ce
Marceli_Nowak <kt2137_69@gmail.com>
2024-05-24 12:41:32 +0200

    MN PIATEK24.06.2024

--- AWWW_lab4_gr2/SchoolRegister.Model/DataModels/Student.cs
+++ AWWW_lab4_gr2/SchoolRegister.Model/DataModels/Student.cs
@@ -2,32 +2,76 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolRegister.Model.DataModels
 {
-    public class Student:User
+    public class Student : User
     {
-        public Group Group{get;set;}
-        public int GroupId{get;set;}
-        public IList<Grade> Grades{get;set;}
-        public Parent Parent{get;set;}
-        public int? ParentId{get;set;}
-        public double AverageGrade{get{
-            double average = 0;
-            foreach(Grade g in Grades){
-                average+=(double)g.GradeValue;
+        public virtual Group? Group { get; set; }
+        [ForeignKey("Group")]
+        public int? GroupId { get; set; }
+        public virtual IList<Grade> Grades { get; set; } = default!;
+        public virtual Parent? Parent { get; set; }
+        [ForeignKey("Parent")]
+        public int? ParentId { get; set; }
+
+        [NotMapped]
+        public double AverageGrade
+        {
+            get
+            {
+                if (Grades == null || Grades.Count == 0)
+                    return 0;
+                return Grades.Average(g => (double)g.GradeValue);
             }
-            return average/Grades.Count;
-        }}
-        public IDictionary<string, double> AverageGradePerSubject{get{
-            IDictionary<string, double> Lista = new Dictionary<string, double>();
-            foreach(Grade g in Grades){
-                Lista.Add(g.Subject.Name, (double)g.GradeValue);
+        }
+
+        [NotMapped]
+        public IDictionary<string, double> CalculateAverageGradePerSubject
+        {
+            get
+            {
+                var subjectAverages = new Dictionary<string, double>();
+                var subjectGroups = Grades.GroupBy(g => g.Subject.Name);
+
+                foreach (var group in subjectGroups)
+                {
+                    subjectAverages[group.Key] = group.Average(g => (double)g.GradeValue);
+                }
+
+                return subjectAverages;
             }
-            return Lista;
-        }}
-        public IDictionary<string, List<GradeScale>>? GradesPerSubject{get{
-            return null;
-        }}
+        }
+
+        public IDictionary<string, double> CalculateAllAverageGradesPerSubject()
+        {
+            var averageGradesPerSubject = new Dictionary<string, double>();
+
+            var subjects = Grades.Select(g => g.Subject.Name).Distinct();
+            foreach (var subject in subjects)
+            {
+                averageGradesPerSubject[subject] = CalculateAverageGradePerSubject[subject];
+            }
+
+            return averageGradesPerSubject;
+        }
+
+        public IDictionary<string, List<GradeScale>> GetGradesPerSubject()
+        {
+            var gradesPerSubject = new Dictionary<string, List<GradeScale>>();
+
+            foreach (var grade in Grades)
+            {
+                if (!gradesPerSubject.ContainsKey(grade.Subject.Name))
+                {
+                    gradesPerSubject[grade.Subject.Name] = new List<GradeScale>();
+                }
+                gradesPerSubject[grade.Subject.Name].Add(grade.GradeValue);
+            }
+
+            return gradesPerSubject;
+        }
     }
 }
--- AWWW_lab4_gr2/SchoolRegister.Model/DataModels/User.cs
+++ AWWW_lab4_gr2/SchoolRegister.Model/DataModels/User.cs
@@ -1,9 +1,16 @@
 using Microsoft.AspNetCore.Identity;
 using System;
 namespace SchoolRegister.Model.DataModels;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
 public class User : IdentityUser<int>
 {
+    [Required]
     public string FirstName { get; set; } = null!;
+    [Required]
     public string LastName { get; set; } = null!;
     public DateTime RegistrationDate { get; set; } = DateTime.Now;
 }
