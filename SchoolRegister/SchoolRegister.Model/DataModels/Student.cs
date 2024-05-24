8e161f7dbd3e876bcfdf2e543b20da47fb3fe637
KamilKopischke <a@a.a>
2024-05-24 11:17:17 +0200

    asdasdasd

--- SchoolRegister/SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister/SchoolRegister.Model/DataModels/Student.cs
@@ -10,10 +10,32 @@ namespace SchoolRegister.Model.DataModels
         public virtual Group? Group{get;set;}
         public int? GroupId {get;set;}
         public virtual IList<Grade>? Grades {get;set;}
-        public virtual Parent? Parent {get;set;}
+        public virtual Parent Parent {get;set;} = null!;
         public int? ParentId {get;set;}
-        public double? AverageGrade {get;}
-        public IDictionary<string,double>? AverageGradePerSubject {get;}
-        public IDictionary<string,List<GradeScale>>? GradesPerSubject {get;}
+        public double? AverageGrade {get {return Grades?.Select(x=>((int)x.GradeValue)).Average();}}
+        public IDictionary<string,double>? AverageGradePerSubject {get {
+            var dict = new Dictionary<string,double>();
+            if (Grades == null || !Grades.Any()){
+                return dict;
+            }
+            var subjectList = Grades.Select(x=>x.Subject).Distinct();
+            foreach (var subject in subjectList)
+            {
+                dict.Add(subject.Name,Grades.Where(x=>x.Subject==subject).Select(x=>(int)x.GradeValue).Average());
+            }
+            return dict;
+            }}
+        public IDictionary<string,List<GradeScale>>? GradesPerSubject {get {
+            var dict = new Dictionary<string,List<GradeScale>>();
+            if (Grades == null || !Grades.Any()){
+                return dict;
+            }
+            var subjectList = Grades.Select(x=>x.Subject).Distinct();
+            foreach (var subject in subjectList)
+            {
+                dict.Add(subject.Name,Grades.Where(x=>x.Subject==subject).Select(x=>x.GradeValue).ToList());
+            }
+            return dict;
+            }}
     }
 }
