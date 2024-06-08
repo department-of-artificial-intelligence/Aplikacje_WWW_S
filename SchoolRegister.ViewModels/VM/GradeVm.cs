ced5ca2464385e7d328a820bbd9595066afe221b
Nazar <fsdfdsf@gmail.com>
2024-06-08 23:18:41 +0200

    lab7_begin

--- SchoolRegister.ViewModels/VM/GradeVm.cs
+++ SchoolRegister.ViewModels/VM/GradeVm.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 using SchoolRegister.Model.DataModels;
+
 namespace SchoolRegister.ViewModels.VM;
+
 public class GradeVm
 {
     public int Value { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public DateTime DateOfIssue { get; set; }
     public int StudentId { get; set; }
     public int SubjectId { get; set; }
--- SchoolRegister.Web/Program.cs
+++ SchoolRegister.Web/Program.cs
@@ -1,41 +1,62 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
 using SchoolRegister.DAL.EF;
 using SchoolRegister.Model.DataModels;
+using SchoolRegister.Web.Controllers;
 using SchoolRegister.Services.Configuration.AutoMapperProfiles;
+using SchoolRegister.Services.Interfaces; // Add this namespace if your service interfaces are here
+using SchoolRegister.Services.ConcreteServices; // Add this namespace if your service implementations are here
+
 var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddAutoMapper(typeof(MainProfile));
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString)
+);
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
-.AddRoles<Role>()
-.AddRoleManager<RoleManager<Role>>()
-.AddUserManager<UserManager<User>>()
-.AddEntityFrameworkStores<ApplicationDbContext>();
+builder
+    .Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<Role>()
+    .AddRoleManager<RoleManager<Role>>()
+    .AddUserManager<UserManager<User>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddTransient(typeof(ILogger), typeof(Logger<Program>));
+builder.Services.AddScoped<IStringLocalizer, StringLocalizer<BaseService>>();
+builder.Services.AddScoped<ISubjectService, SubjectService>();
+builder.Services.AddScoped<IGradeService, GradeService>();
+builder.Services.AddScoped<IGroupService, GroupService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<ITeacherService, TeacherService>();
+
 builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-app.UseMigrationsEndPoint();
+    app.UseMigrationsEndPoint();
 }
 else
 {
-app.UseExceptionHandler("/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcorehsts.
-app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcorehsts.
+    app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllerRoute(
-name: "default",
-pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
 app.Run();
