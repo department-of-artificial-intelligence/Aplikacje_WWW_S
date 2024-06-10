a75a24d3ef2ba8c6373b8ab3eaa4270ec7f343ed
Rafal Grycuk <smth@smth.smth>
2024-06-10 11:38:18 +0200

    Issue fixed

--- Kolokwium/Kolokwium.Services/ConcreteServices/StudentService.cs
+++ Kolokwium/Kolokwium.Services/ConcreteServices/StudentService.cs
@@ -17,21 +17,29 @@ public class StudentService : BaseService, IStudentService
     public StudentVm AddOrUpdateStudent(AddOrUpdateStudentVm addOrUpdateStudentVm)
     {
         try {
-            if(addOrUpdateStudentVm == null) throw new ArgumentNullException(); 
-
-            var studentEntity = Mapper.Map<Student>(addOrUpdateStudentVm); 
-            if(!addOrUpdateStudentVm.Id.HasValue || addOrUpdateStudentVm.Id == 0){
-                DbContext.Students.Add(studentEntity); 
-            } else {
-                var studentToEdit = DbContext.Students.Find(addOrUpdateStudentVm.Id); 
-                if(studentToEdit!= null){
-                    studentToEdit.FirstName = addOrUpdateStudentVm.FirstName; 
-                    studentToEdit.LastName = addOrUpdateStudentVm.LastName; 
-                    studentToEdit.Grades = addOrUpdateStudentVm.Grades; 
-                } 
+            if(addOrUpdateStudentVm == null) throw new ArgumentNullException($"{nameof(addOrUpdateStudentVm)} is null");
+            Student studentEntity;
+            if (addOrUpdateStudentVm.Id.HasValue && addOrUpdateStudentVm.Id > 0)
+            {
+                var studentEntityToEdit = DbContext.Students.FirstOrDefault(s => s.Id == addOrUpdateStudentVm.Id);
+                if (studentEntityToEdit != null)
+                {
+                    studentEntity = Mapper.Map<AddOrUpdateStudentVm, Student>(addOrUpdateStudentVm, studentEntityToEdit);
+                    DbContext.Students.Update(studentEntity);
+                }
+                else throw new Exception("Entity not exists");
+                //var studentToEdit = DbContext.Students.Find(addOrUpdateStudentVm.Id); 
+                //if(studentToEdit!= null){
+                //    studentToEdit.FirstName = addOrUpdateStudentVm.FirstName; 
+                //    studentToEdit.LastName = addOrUpdateStudentVm.LastName; 
+                //    studentToEdit.Grades = addOrUpdateStudentVm.Grades; 
+                //} 
+            }
+            else {
+                studentEntity = Mapper.Map<Student>(addOrUpdateStudentVm);
+                DbContext.Students.Add(studentEntity);
             }
             DbContext.SaveChanges(); 
-
             var studentVm = Mapper.Map<StudentVm>(studentEntity); 
             return studentVm; 
 
--- Kolokwium/Kolokwium.ViewModel/VM/AddOrUpdateStudentVm.cs
+++ Kolokwium/Kolokwium.ViewModel/VM/AddOrUpdateStudentVm.cs
@@ -8,5 +8,5 @@ public class AddOrUpdateStudentVm {
     public int? Id {get; set;}
     public required string FirstName {get; set;}
     public required string LastName {get; set;}
-    public IEnumerable<Grade>? Grades {get; set;}
+   // public IEnumerable<Grade>? Grades {get; set;}
 }
