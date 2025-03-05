71b86245d6d3fc2a20be4094fec67438782bc870
Bartosz Mazur <az@gmail.com>
2025-03-05 15:44:53 +0100

    Lab1

---
+++ AWWW_lab1_gr3/Models/Articles.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWWW_lab1_gr3.Models
+{
+    public class Articles
+    {
+        
+    }
+}
