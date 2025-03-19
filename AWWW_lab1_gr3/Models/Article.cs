9436c2fadd21bb7307dd1ee3b0fe70daf0ce1150
Adam Radecki <smth@smth.smth>
2025-03-19 14:18:32 +0100

    lab1_full

--- AWWW_lab1_gr3/Models/Article.cs
+++ AWWW_lab1_gr3/Models/Article.cs
@@ -6,6 +6,6 @@ public class Article
     public int Id {get; set; }
     public string Title { get; set; }
     public string Content {get; set; }
-    public DateTime CreateDate { get; set; }
+    public DateTime CreationDate { get; set; }
 
 }
