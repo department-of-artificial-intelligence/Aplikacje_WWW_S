4c16c9a6345df09b905ebf275c6d8e3ccd3f00a6
inzanty <inzanty@gmail.com>
2025-04-14 11:09:16 +0200

    Lab22222

--- AWWW_lab02_gr3/Models/Article.cs
+++ AWWW_lab02_gr3/Models/Article.cs
@@ -1,15 +1,20 @@
 namespace AWWW_lab02_gr3.Models
 {
     public class Article
-
     {
         public int Id {get; set; }
         public string Title { get; set; } = null!;
         public string Content {get; set; } = null!;
         public DateTime CreationDate { get; set; }
-
+        
         public ICollection<Comment>? Comments {get; set;}
-
+//po to zeby odnalezc sie w swoim kodzie
+//nie warto, kiedy jest to maly projekt (nie bedzie rozwijany)
+//dal - komunikacja z wartwa prezentacji i komunikacji
+//viewmodel - 
+//services - aplikacja webowa + api, jedna metoda, ktora bedzie wyswietlac srednia ocen bedzie z api i z weba. dostępne w wielu miejscach
+//test- fremweork testujacy uslugi (viewmodele i services). umozliwia testowanie funkcjonalnosci (czy sredni sie dobrze policzyla, itd.)
+//web(app mvc) - dostep do wszystkiego poza testem (model, viewmodel, services, dal).
         public ICollection<Tag>? Tags {get; set;}
 
         public int AuthorId {get; set;}
@@ -20,6 +25,5 @@ namespace AWWW_lab02_gr3.Models
         
         public int? MatchId {get; set;}
         public Match? Match {get; set;}
-        
     }
 }
