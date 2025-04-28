734df2f79a0de700949b117f6ec708767328c17b
Jakub Grzybowski <dfvfd>
2025-04-28 09:36:41 +0200

    CD

--- Kolokwium/Controllers/CarController.cs
+++ Kolokwium/Controllers/CarController.cs
@@ -16,4 +16,35 @@ public class CarController : Controller{
         var car = _context.Cars.Include(o => o.Owners).ToList();
         return View(car);
     }
+
+    public IActionResult Add()
+    {
+        ViewBag.owners = _context.Owners.Select(o => new SelectListItem
+        {
+            Value = o.Id.ToString(),
+            Text = o.Name
+        }).ToList();
+        ViewBag.garages = _context.Garages.Select(g => new SelectListItem
+        {
+            Value = g.Id.ToString(),
+            Text = g.Nazwa
+        }).ToList();
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Car car , List<int> owners)
+    {
+       var ownerss = owners != null ? _context.Owners.Where(o => owners.Contains(o.Id)).ToList() : new List<Owner>();
+       car.Owners = ownerss;
+
+       var garage = _context.Garages.FirstOrDefault(g => g.Id == car.GarageId);
+       car.Garage = garage;
+
+        _context.Cars.Add(car);
+        _context.SaveChanges();
+
+
+        return RedirectToAction("Index");
+    }
 }
--- Kolokwium/Program.cs
+++ Kolokwium/Program.cs
@@ -2,13 +2,35 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
+
 builder.Services.AddDbContext<MyDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbContext")));
 
 builder.Services.AddControllersWithViews();
+
+
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+// Add services to the container.
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
-app.MapDefaultControllerRoute(); 
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
