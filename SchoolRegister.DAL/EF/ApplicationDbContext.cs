77c346fb5b29f413504ea7d66d0b70fbcfe55b27
Nazar_Cheva <wykadota@outlook.com>
2025-04-29 21:14:06 +0200

    lab5_started

---
+++ SchoolRegister.DAL/EF/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore; 
+using SchoolRegister.Model.DataModels; 
+public class ApplicationDbContext : IdentityDbContext<User, Role, int>
+{
+// table properties
+public DbSet<Grade> Grades { get; set; }
+public DbSet<Group> Groups { get; set; }
+public DbSet<Subject> Subjects { get; set; }
+public DbSet<SubjectGroup> SubjectGroups { get; set; }
+public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+: base(options) { }
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+base.OnConfiguring(optionsBuilder);
+//configuration commands
+optionsBuilder.UseLazyLoadingProxies(); //enable lazy loading proxies
+}
+protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+base.OnModelCreating(modelBuilder);
+// Fluent API commands
+modelBuilder.Entity<User>()
+.ToTable("AspNetUsers")
+.HasDiscriminator<int>("UserType")
+.HasValue<User>((int)RoleValue.User)
+.HasValue<Student>((int)RoleValue.Student)
+.HasValue<Parent>((int)RoleValue.Parent)
+.HasValue<Teacher>((int)RoleValue.Teacher);
+}
+}
