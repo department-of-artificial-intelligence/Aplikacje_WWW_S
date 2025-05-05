5650e0350007df4716e5e2afcd36ab6d96313476
Rog_Krzysztof <dsaf@gmail.com>
2025-05-05 03:44:14 -0600

    lab5

--- AWWW_lab5_gr3/SchoolRegister.DAL/EF/ApplicationDbContext.cs
+++ AWWW_lab5_gr3/SchoolRegister.DAL/EF/ApplicationDbContext.cs
@@ -18,7 +18,6 @@ optionsBuilder.UseLazyLoadingProxies(); //enable lazy loading proxies
 protected override void OnModelCreating(ModelBuilder modelBuilder)
 {
 base.OnModelCreating(modelBuilder);
-
 // Fluent API commands
 modelBuilder.Entity<User>()
 .ToTable("AspNetUsers")
@@ -27,5 +26,16 @@ modelBuilder.Entity<User>()
 .HasValue<Student>((int)RoleValue.Student)
 .HasValue<Parent>((int)RoleValue.Parent)
 .HasValue<Teacher>((int)RoleValue.Teacher);
+modelBuilder.Entity<SubjectGroup>()
+.HasKey(sg => new { sg.GroupId, sg.SubjectId });
+modelBuilder.Entity<SubjectGroup>()
+.HasOne(g => g.Group)
+.WithMany(sg => sg.SubjectGroups)
+.HasForeignKey(g => g.GroupId);
+modelBuilder.Entity<SubjectGroup>()
+.HasOne(s => s.Subject)
+.WithMany(sg => sg.SubjectGroups)
+.HasForeignKey(s => s.SubjectId)
+.OnDelete(DeleteBehavior.Restrict);
 }
 }
