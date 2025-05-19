8de774d5ea4cd04c3a0b1ac27af8bf78b38dd357
PiotrW <kik@wp.pl>
2025-05-19 11:19:49 +0200

    Lab6

---
+++ AWWW_lab4_gr3/SchoolRegister.Model/DataModels/SubjectGroup.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolRegister.Model.DataModels;
+
+public class SubjectGroup
+{
+    public virtual Subject? Subject {get; set;}
+    [ForeignKey("Subject")]
+    public int? SubjectId {get; set;}
+    public virtual Group? Group {get; set;}
+    [ForeignKey("Group")]
+    public int? GroupId { get; set;}
+    public SubjectGroup(){}
+}
