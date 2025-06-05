21bf698cc595402ef8d6360f0755732ad70d3f56
Jakub Grzybowski <dfvfd>
2025-06-05 21:18:05 +0200

    kolo 2

---
+++ Aplikacje_WWW_S-kolokwium2/Kolokwium/Kolokwium.ViewModel/VM/LoginUserVm.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kolokwium.ViewModel.VM;
+public class LoginUserVm
+{
+    [Required]
+    [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [Display(Name = "Email")]
+    public string Login { get; set; } = null!;
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+}
