c73fe6c52a6ed3ad7474a84195741bf36a0ed126
Bartlomiej Latocha <coscos@gmail.com>
2026-03-03 11:48:46 +0100

    lab1.2

---
+++ AWWW_lab1_gr1/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
+namespace AWWW_lab1_gr1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+    }
+}
