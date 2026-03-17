572bf520403b621d46ca9516bf7644b7f4afadfb
Dominik <niema@niema.com>
2026-03-17 01:34:36 +0100

    Zapis2

--- AWWW_lab1_gr1/Controllers/ArticleController.cs
+++ AWWW_lab1_gr1/Controllers/ArticleController.cs
@@ -5,17 +5,31 @@ namespace AWWW_lab1_gr1.Controllers
 {
     public class ArticleController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int id=1)
         {
-            var article = new Article
+            var article = new List<Article>
             {
+            new Article{
                 Id = 1,
                 Title = "Artykuł 1",
                 Content = "Przykładowy tekst",
                 CreationDate = DateTime.Now
-            };
+            }, 
+            new Article{
+                Id = 1,
+                Title = "Artykuł 1",
+                Content = "Przykładowy tekst",
+                CreationDate = DateTime.Now
+            }, 
+            new Article{
+                Id = 1,
+                Title = "Artykuł 1",
+                Content = "Przykładowy tekst",
+                CreationDate = DateTime.Now
+            }
+        };
             return View(article);
-        }
+    }
     }
 }
 
