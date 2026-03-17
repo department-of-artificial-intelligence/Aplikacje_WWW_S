471751bc678ca20ada9c8707e645f581031971f8
Bartlomiej Latocha <coscos@gmail.com>
2026-03-17 11:48:05 +0100

    Lab2

---
+++ AWWW_lab2_gr1/Models/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace AWWW_lab2_gr1.Models
+{
+	public class OrderItem
+	{
+		public int Id { get; set; }
+
+		public int OrderId { get; set; }
+	}
+}
---
+++ AWWW_lab2_gr1/Program.cs
@@ -0,0 +1,33 @@
+namespace AWWW_lab2_gr1
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			builder.Services.AddControllersWithViews();
+
+			var app = builder.Build();
+
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
+}
