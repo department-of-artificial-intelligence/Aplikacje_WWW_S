f035598ed8842359173600aec609bcc3d30f97d7
omgomg <omgomg@omg.omg>
2026-03-17 23:36:50 +0100

    Lab 2 Finished CRUD for Category

--- AWWW_lab2_gr1/Controllers/CategoryController.cs
+++ AWWW_lab2_gr1/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using AWWW_lab2_gr1.Models;
 using Microsoft.VisualBasic;
 
@@ -17,9 +18,58 @@ namespace AWWW_lab2_gr1.Controllers
             _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index() //R
         {
            return View(_db.Categories.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id) //D
+        {
+            var category = _db.Categories.Find(id);
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Create() //C
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateConfirmed(Category category)
+        {
+            if (category != null)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int id) //U
+        {
+            var category = _db.Categories.Find(id);
+            if (category != null)
+            {
+                return View(category);
+            }
+             return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult UpdateConfirmed(Category category)
+        {
+            if (category != null)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
