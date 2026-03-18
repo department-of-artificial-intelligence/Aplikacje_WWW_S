fcdce11f5d665739d94601e1ff391a315536fa04
Dominik <niema@niema.com>
2026-03-18 21:39:09 +0100

    Lab1v2

---
+++ AWWW_lab1.v2_gr1/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWWW_lab1.v2_gr1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+    }
+}
