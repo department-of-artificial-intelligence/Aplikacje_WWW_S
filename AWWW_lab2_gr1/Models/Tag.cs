855ded94e251daa304c444103b1a0701f13ad01f
username <username@username.com>
2026-03-19 19:36:22 +0100

    w domu 19 marca

--- AWWW_lab2_gr1/Models/Tag.cs
+++
@@ -1,9 +0,0 @@
-namespace AWWW_lab2_gr1.Models;
-
-public class Tag
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    public ICollection<Product> Products { get; set; }
-}
