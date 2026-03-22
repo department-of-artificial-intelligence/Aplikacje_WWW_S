50e752ecf340d6a2de42491472bdee786038f418
Radoslaw_Kawa <smth@smth.smth>
2026-03-22 01:53:24 +0100

    Lab2 poprawka v1

---
+++ AWWW_lab2_gr1/AWWW_lab2_gr1/Migrations/20260322004958_MakeCustomerIdNullable.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AWWW_lab2_gr1.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCustomerIdNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Addresses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Street",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostalCode",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerId",
+                table: "Addresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Addresses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_Customers_CustomerId",
+                table: "Addresses",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- AWWW_lab2_gr1/AWWW_lab2_gr1/Models/Address.cs
+++ AWWW_lab2_gr1/AWWW_lab2_gr1/Models/Address.cs
@@ -2,12 +2,12 @@
 {
     public class Address
     {
-        public int Id { get; set; }
-        public int CustomerId { get; set; }
+        public int? Id { get; set; } //nulle bo nie ma klientow z ktorymi powiazana jest baza i sie psulo
+        public int? CustomerId { get; set; }
         public string City { get; set; }
         public string Street { get; set; }
         public string PostalCode { get; set; } // 
 
-        public Customer Customer { get; set; } //referencja do 1 Customer
+        public Customer? Customer { get; set; } //referencja do 1 Customer
     }
 }
