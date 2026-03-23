fceb284dafdbc0533899027984e312ebb2f7156a
Szymon_Profic <smth@smth.com>
2026-03-23 20:45:09 +0100

    Added Tag controller

--- AWWW_lab2_gr1/lab2/Controllers/Tag/TagController.cs
+++ AWWW_lab2_gr1/lab2/Controllers/Tag/TagController.cs
@@ -3,12 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using lab2.Models;
+using lab2.Data;
 
-
-namespace lab2.Controllers.Tag
+namespace lab2.Controllers
 {
     public class TagController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View(FakeData.Tags);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Tag tag)
+        {
+            if (!ModelState.IsValid)
+                return View(tag);
+
+            tag.Id = FakeData.Tags.Count == 0
+                ? 1
+                : FakeData.Tags.Max(c => c.Id) + 1;
+
+            FakeData.Tags.Add(tag);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- AWWW_lab2_gr1/lab2/Models/Tag.cs
+++ AWWW_lab2_gr1/lab2/Models/Tag.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace lab2.Models
 {
-
     public class Tag
     {
         public int Id { get; set; }
