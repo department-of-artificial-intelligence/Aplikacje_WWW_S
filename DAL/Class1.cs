9e9c9f9e6ea7b1fceb98c4276549db2baf4f6eeb
Andriy Babyuk <smth@smth.smth>
2026-04-21 11:43:19 +0200

    Lab 5 Initial (Recreated)

---
+++ DAL/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace DAL {
+    public class Class1 {
+
+    }
+}
---
+++ Web/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Web.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
