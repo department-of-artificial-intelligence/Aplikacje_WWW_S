df6d86babd4a1a7f300d7ed91436709d8f5fab5a
Andriy Babyuk <smth@smth.smth>
2026-04-28 11:48:18 +0200

    Aabb

--- DAL/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace DAL {
-    public class Class1 {
-
-    }
-}
---
+++ Model/RoomEquipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model {
+    public class RoomEquipment {
+        public int Id { get; set; }
+
+        [ForeignKey("Room")]
+        public int RoomId { get; set; }
+
+        public virtual Room Room { get; set; }
+
+        [ForeignKey("Equipment")]
+        public int EquipmentId { get; set; }
+        public virtual Equipment Equipment { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
