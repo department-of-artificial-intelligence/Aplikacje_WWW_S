1733ac3631edf7d95992e18830c289e7809435a2
Radoslaw Kawa <cos@cos.cosiek>
2026-04-28 11:44:09 +0200

    poczatek lab6

---
+++ AWWW_lab5_gr1/Services/DTO/Building/UpdateBuildingDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Proxies.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.DTO.Building
+{
+    internal class UpdateBuildingDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string? Description { get; set; }
+    }
+}
---
+++ AWWW_lab5_gr1/Services/Services/BaseService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.EF;
+
+namespace Services.Services
+{
+    public abstract class BaseService
+    {
+        protected readonly AppDbContext _dbContext;
+
+        public BaseService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
