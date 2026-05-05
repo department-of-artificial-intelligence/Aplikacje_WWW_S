a19d8cd5613ba6c922c4a5a239cf5876a4789224
Pawel Szczypior <smth@smth.smth>
2026-05-05 11:43:50 +0200

    lab7_poczatek

---
+++ AWWW_lab5_gr1/Services/DTO/RoomEquipment/RoomEquipment.cs
@@ -0,0 +1,25 @@
+﻿namespace Services.DTO.RoomEquipment
+{
+    public class CreateRoomEquipmentDto
+    {
+        public int RoomId { get; set; }
+        public int EquipmentId { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class UpdateRoomEquipmentDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class RoomEquipmentItemDto
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public string RoomName { get; set; } = null!;
+        public int EquipmentId { get; set; }
+        public string EquipmentName { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ AWWW_lab5_gr1/Services/Interfaces/IRoomEquipmentService.cs
@@ -0,0 +1,19 @@
+﻿using Services.DTO.RoomEquipment;
+
+namespace Services.Interfaces
+{
+    public interface IRoomEquipmentService
+    {
+        Task<List<RoomEquipmentItemDto>> GetAllSync();
+
+        Task<List<RoomEquipmentItemDto>> GetByRoomId(int roomId);
+
+        Task<RoomEquipmentItemDto?> GetByIdAsync(int id);
+
+        Task<int> CreateAsync(CreateRoomEquipmentDto dto);
+
+        Task<bool> UpdateAsync(UpdateRoomEquipmentDto dto);
+
+        Task<bool> DeleteAsync(int id);
+    }
+}
