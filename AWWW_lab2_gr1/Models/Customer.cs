b44b8672403a6648df6e68ef29a7fd77e1b4b57f
Bartlomiej Latocha <coscos@gmail.com>
2026-05-05 11:44:44 +0200

    Lab6

--- AWWW_lab2_gr1/Models/Customer.cs
+++ AWWW_lab2_gr1/Models/Customer.cs
@@ -4,7 +4,6 @@
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-
 		public CustomerProfile CustomerProfile { get; set; }
 		public List<Address> Addresses { get; set; }
 		public List<Order> Orders { get; set; }
--- AWWW_lab5_gr1/Services/DTO/RoomEquipment/CreateRoomEquipmentDto.cs
+++ AWWW_lab5_gr1/Services/DTO/RoomEquipment/CreateRoomEquipmentDto.cs
@@ -6,13 +6,10 @@ using System.Threading.Tasks;
 
 namespace Services.DTO.RoomEquipment
 {
-    public class RoomEquipmentItemDto
+    public class CreateRoomEquipmentDto
     {
-        public int Id { get; set; }
         public int RoomId { get; set; }
-        public string RoomName { get; set; } = null!;
         public int EquipmentId { get; set; }
-        public string EquipmentName { get; set; } = null!;
         public int Quantity { get; set; }
     }
 }
